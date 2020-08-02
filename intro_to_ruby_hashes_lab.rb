4bf3c0b04864e1e5042287f78f26bf9eb65c05ef
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-02 00:17:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,7 +59,8 @@ hash = {
     },
     :names => {
       :reading_railroad => {
-        :
+        :mortgage_value => 100,
+        
       },
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
